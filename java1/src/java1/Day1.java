e8672ad15c95e0423dbf8349158059042946c537
Ritika Ghuge <ritikaghuge1811@gmail.com>
2026-07-28 15:35:21 +0530

    Day1

---
+++ java1/src/java1/Day1.java
@@ -0,0 +1,14 @@
+package java1;
+
+public class Day1 {
+	public static void main(String[] args) {
+		//1. Write a Java program to display your personal information, including your name, age, city, and 
+		//qualification.
+		System.out.println("name-Ritika");
+		System.out.println("age-20");
+		System.out.println("city-Buldana");
+		System.out.println("qualification-pursuing Btech");
+	}
+	
+
+}
--- java1/src/java1/JavaPro.java
+++ java1/src/java1/JavaPro.java
@@ -2,8 +2,6 @@ package java1;
 
 public class JavaPro {
 	public static void main(String[] args) {
-		System.out.println("ritika");		
+		System.out.println("ritika");
 	}
-	
-
 }
